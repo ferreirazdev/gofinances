b669723e35840c55f9237243a01621874803c420
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-07-17 14:21:25 -0300

    Adding routes and styling navigation bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 import { 
   useFonts,
@@ -10,8 +14,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,8 +29,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Dashboard /> */}
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
